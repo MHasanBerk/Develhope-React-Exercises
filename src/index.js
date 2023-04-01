80486b522b4a517b0d89c2c25238180ab2853658
Hasan Berk <hasnberk@outlook.com>
2023-04-01 16:03:06 +0300

    jsx-03 done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import ReactDOM from 'react-dom'
 const root = document.getElementById("root");
 
 function addName(name){
-    return <h1>Hello {name}!</h1>
+    return (
+        <div>
+            <h1>Hello {name}!</h1>
+            <h3>Type of: {typeof(name)}</h3>
+        </div>
+    )
 }
 
-ReactDOM.render(addName("Joe"), root);
+//Without a variable inside the function, it ignores 
+
+ReactDOM.render(addName(), root);
