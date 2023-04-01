78469ac61ca8eb7d506034be5538bef3cb54c9de
Hasan Berk <hasnberk@outlook.com>
2023-04-01 16:47:09 +0300

    components-08 done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ export class App extends React.Component{
         return(
             <div>
                 <HelloWorld />
+                <HelloWorld />
+                <HelloWorld />
+                <HelloWorld />
+                <HelloWorld />
             </div>
         )
     }
 }
+
+// Yes, we can use hello component more than once. For example in this code block,
+// it will render 5 times.
