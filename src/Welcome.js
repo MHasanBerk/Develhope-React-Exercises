503224089f7a9d6ec7d87fa0870ff006c6e7c8ee
Hasan Berk <hasnberk@outlook.com>
2023-04-07 22:47:17 +0300

    cond. ren. 01 done

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component{
         return (
             <div>
                 <p>Hello, {this.props.name}</p>
-                <Age age={25}/>
+                <Age age={18}/>
             </div>
         )
     }
