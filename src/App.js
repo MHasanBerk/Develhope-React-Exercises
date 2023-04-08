857d0dc1b4efc9b536158e7d06500b798d98be91
Hasan Berk <hasnberk@outlook.com>
2023-04-08 07:34:29 +0300

    cond. rend. 05 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,6 @@ import { Welcome } from "./Welcome"
 
 export class App extends React.Component{
     render(){
-        return <Welcome name="John" age={18} />
+        return <Welcome name="John" age={25}/>
     }
 }
