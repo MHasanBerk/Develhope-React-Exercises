e4233f8a3fa05edcbce4cfb2f8634bfea060f6b0
Hasan Berk <hasnberk@outlook.com>
2023-04-10 21:22:45 +0300

    useState-01 done

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,25 +1,41 @@
-import React from "react";
-import { CounterButton } from "./CounterButton";
+import { useState } from "react"
 
-export class ClickCounter extends React.Component{
-    state = {
-        count : 0
-    }
-    
-    handleClick = () => {
-        this.setState((state) => {
-            return{
-                count: state.count + (this.props.incrementBy ?? 1)
-            }
-        })
-    }
+export function ClickCounter({initialValue = 0}){
+    const [counter,setCounter] = useState(initialValue)
 
-    render(){
-        return(
-            <div>
-                <h3>Count with the Button: {this.state.count}</h3>
-                <CounterButton onClick={this.handleClick}/>
-            </div>
-        )
+    function handleCounterIncrement(){
+        setCounter((callbackCounter) => (callbackCounter + 1))
     }
+    return(
+        <div>
+            <h3>Counter: {counter}</h3>
+            <button onClick={handleCounterIncrement}>Increment</button>
+        </div>
+    )
 }
+
+// import React from "react";
+// import { CounterButton } from "./CounterButton";
+
+// export class ClickCounter extends React.Component{
+//     state = {
+//         count : 0
+//     }
+    
+//     handleClick = () => {
+//         this.setState((state) => {
+//             return{
+//                 count: state.count + (this.props.incrementBy ?? 1)
+//             }
+//         })
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <h3>Count with the Button: {this.state.count}</h3>
+//                 <CounterButton onClick={this.handleClick}/>
+//             </div>
+//         )
+//     }
+// }
