6b725533f62546a1803ce01657d993d2e2e6ab8a
Hasan Berk <hasnberk@outlook.com>
2023-04-10 22:37:11 +0300

    useState-02 done

--- src/Login.js
+++ src/Login.js
@@ -1,72 +1,136 @@
-import React from "react";
+import { useState } from "react"
 
-export class Login extends React.Component{
-
-    state = {
-        username : '',
-        password : '',
-        remember : false,
+export function Login(){
+    const [data,setData] = useState({
+        username: "",
+        password: "",
+        remember: false
+    })
+    function handleSubmit(e){
+        e.preventDefault();
     }
 
-    handleInputChange = (event) => {
-        const name = event.target.name;
-        const type = event.target.type;
-        const value = event.target.value;
-        const checked = event.target.checked;
+    function handleInputChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        const type = e.target.type;
+        const checked = e.target.checked;
 
-        this.setState({
-            [name] : type === 'checkbox' ? checked : value
+        setData({
+            ...data,
+            [name]: type === "checkbox" ? checked : value
         })
     }
 
-    onLogin = () => {
-        this.setState({
-            username: this.state.username,
-            password: this.state.password
-        })
-        console.log(this.state)
+    function handleLoginButton(){
+        console.log(data);
     }
 
-    handleReset = () => {
-        this.setState({
-            username : '',
-            password : '',
-            remember : false,
+    function handleResetButton(){
+        setData({
+            username: "",
+            password: "",
+            remember: false
         })
     }
 
-    render(){
-        let isDisabled;
-        
-        if(this.state.username === "" || this.state.password === ""){
-            isDisabled = true;
-        }else{
-            isDisabled = false;
-        }
+    const isDisabled = (data.username === "" || data.password === "") ? true : false
 
-        const styleLogin = {
-            backgroundColor: this.state.password.length < 8 ? "red" : "green",
-            color: "white"
-        }
-        
-        return(
-            <div>
-                <h1>Login Form</h1>
-                <label>
-                    Username:
-                    <input name="username" value={this.state.username} onChange={this.handleInputChange}/>
-                </label>
-                <label>
-                    Password:
-                    <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange}/>
-                </label>
-                <label>
-                    Checkbox:
-                    <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange}/>
-                </label>
-                <button onClick={this.onLogin} disabled={isDisabled} style={styleLogin}>Login</button>
-                <button onClick={this.handleReset}>Reset</button>
-            </div>
-        )
+    const styleLoginButton = {
+        backgroundColor: data.password.length < 8 ? "red" : "green",
+        color: "white"
     }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <h1>Login Form</h1>
+            <label>
+                Username:
+                <input name="username" value={data.username} onChange={handleInputChange}/>
+            </label>
+            <label>
+                Password:
+                <input name="password" type="password" value={data.password} onChange={handleInputChange}/>
+            </label>
+            <label>
+                Remember me
+                <input name="remember" type="checkbox" value={data.remember} onChange={handleInputChange}/>
+            </label>
+            <button onClick={handleLoginButton} disabled={isDisabled} style={styleLoginButton}>Login</button>
+            <button onClick={handleResetButton}>Reset</button>
+        </form>
+    )
 }
+
+// import React from "react";
+
+// export class Login extends React.Component{
+
+//     state = {
+//         username : '',
+//         password : '',
+//         remember : false,
+//     }
+
+//     handleInputChange = (event) => {
+//         const name = event.target.name;
+//         const type = event.target.type;
+//         const value = event.target.value;
+//         const checked = event.target.checked;
+
+//         this.setState({
+//             [name] : type === 'checkbox' ? checked : value
+//         })
+//     }
+
+//     onLogin = () => {
+//         this.setState({
+//             username: this.state.username,
+//             password: this.state.password
+//         })
+//         console.log(this.state)
+//     }
+
+//     handleReset = () => {
+//         this.setState({
+//             username : '',
+//             password : '',
+//             remember : false,
+//         })
+//     }
+
+//     render(){
+//         let isDisabled;
+        
+//         if(this.state.username === "" || this.state.password === ""){
+//             isDisabled = true;
+//         }else{
+//             isDisabled = false;
+//         }
+
+//         const styleLogin = {
+//             backgroundColor: this.state.password.length < 8 ? "red" : "green",
+//             color: "white"
+//         }
+        
+//         return(
+//             <div>
+//                 <h1>Login Form</h1>
+//                 <label>
+//                     Username:
+//                     <input name="username" value={this.state.username} onChange={this.handleInputChange}/>
+//                 </label>
+//                 <label>
+//                     Password:
+//                     <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange}/>
+//                 </label>
+//                 <label>
+//                     Checkbox:
+//                     <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange}/>
+//                 </label>
+//                 <button onClick={this.onLogin} disabled={isDisabled} style={styleLogin}>Login</button>
+//                 <button onClick={this.handleReset}>Reset</button>
+//             </div>
+//         )
+//     }
+// }
