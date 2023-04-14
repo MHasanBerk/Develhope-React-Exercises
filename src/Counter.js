f873009eb28bf08e1a804405813c7229fcd4b498
Hasan Berk <hasnberk@outlook.com>
2023-04-14 05:20:32 +0300

    useEffect-02 done

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,42 @@
-import React from "react"
+import { useEffect, useState } from "react";
 
-export class Counter extends React.Component{
-    state = {
-        count : 0
-    }
+export function Counter(){
+    const [count,setCount] = useState(0);
 
-    constructor(props){
-        super(props)
+    useEffect(() => {
+        const timerID = setInterval(() => {
+            setCount(c => c + 1)
+        },1000);
 
-        setInterval(() => {
-            this.setState((state) => {return {count: state.count + 1}})
-        },1000)
-    }
-    
-    render(){
-        return(
-            <div>
-                <h1>count: {this.state.count}</h1>
-            </div>
-        )
-    }
+        
+        return () => {clearInterval(timerID)}
+
+    },[]);
+
+    return <h1>Count: {count}</h1>
 }
+
+
+// import React from "react"
+
+// export class Counter extends React.Component{
+//     state = {
+//         count : 0
+//     }
+
+//     constructor(props){
+//         super(props)
+
+//         setInterval(() => {
+//             this.setState((state) => {return {count: state.count + 1}})
+//         },1000)
+//     }
+    
+//     render(){
+//         return(
+//             <div>
+//                 <h1>count: {this.state.count}</h1>
+//             </div>
+//         )
+//     }
+//}
